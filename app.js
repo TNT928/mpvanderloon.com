d49d993c30e7d39fd82d253ce950aff9341d4eef
TNT928 <jenkins92886@gmail.com>
2019-05-19 14:31:42 -0400

    configured contact form

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express= require("express")
 const app= express();
 const nodemailer= require('nodemailer');
-const xoauth2= require('xoauth2');
+
 const bodyParser=require('body-parser');
+
 const port =3000;
+require('dotenv').config()
 
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set("view engine", "ejs");
 
@@ -42,63 +46,67 @@ app.get('/contact',(req, res, next)=>{
     res.render('contact')
 })
 
-app.post('/contact',(req,res)=>{
-    const output = `
-    <p>You have a new contact</p>
-    <h3>Contact Details</h3>
-    <ul>
-    <li>Name: ${req.body.name}</li>
-    <li>Email:${req.body.email}</li>
-    <li>Message:${req.body.message}</li>
-    </ul>
-    
-    `
-async function main(){
-
-let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        type: 'OAuth2',
-        clientId: process.env.ID,
-        clientSecret: process.env.SECRET
-        
-    },
-   
-});
-
-let info = await transporter.sendMail({
-    from: '"Michael" <jenkins92886@gmail.com>', // sender address
-    to: "jenkins92886@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>",
-    auth:{
-        user: 'jenkins92886@gmail.com',
-        refreshToken: process.env.REFRESH,
-        accessToken: process.env.ACCESS,
-        expires: 3600
-
-    } 
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-main().catch(console.error);
-
-
+app.get('/success',(req, res)=>{
+    res.render('success')
 })
 
+app.post('/contact',urlencodedParser,(req,res)=>{
+    const output= `
+
+    <p>You have a new contact request</p>
+
+    <h3>Contact Details</h3>
+
+    <ul>
+         <li> Name: ${req.body.Name}</li>
+         <li> Email: ${req.body.Email}</li>
+        <li> Message: ${req.body.Message}</li>
+    
+    </ul>
+    
+    `;
 
 
+    async function main(){
 
+    
+  
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        
+        auth: {
+            user:'jenkins92886@gmail.com',
+            pass: process.env.PASSWORD
+            
+        },
+       
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: 'Node Mailer', // sender address
+      to: "jenkins92886@gmail.com", // list of receivers
+      subject: "New email for M.P. VanderLoon", // Subject line
+      html: output
+      // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
+  
+  main().catch(console.error);
+ 
+
+res.redirect('/success')
+
+})
 
 
 
